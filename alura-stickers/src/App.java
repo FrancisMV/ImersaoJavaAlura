233a32da0b3737ba1bb3e12eabc026b104f0f449
Francis <101139513+FrancisMV@users.noreply.github.com>
2023-03-29 22:21:00 -0300

    Add files via upload

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -19,6 +23,8 @@ public class App {
         // exemplo String imdbKey = Sustem.getenv("IMDB_API+KEY");
         // exemplo String url = "https://imdb-api.com/en/API/Top250Movies" + imdbKey;
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url =
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -31,19 +37,48 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         // System.out.println(listaDeFilmes.size()); //quantidade de resultados(itens)
         // System.out.println(listaDeFilmes.get(0)); // da lista pegar o item 0
-
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
         // exibir e manipular os dados
         // for (Map<String, String> filme : listaDeFilmes) { //listar todos itens
-        for (int i = 0; i < 3; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i);
-            System.out.println("\u001b[1mTítulo:\u001b[0m " + filme.get("title"));
-            System.out.println("\u001b[3mURL da imagem:\u001b[0m " + filme.get("image"));
-            // System.out.println(filme.get("imDbRating")); //valor recebido da API
+        var geradora = new GeradoraDeFigurinhas();
+        // for (int index = 0; index < 5; index++) {
+        // var filme = listaDeFilmes.get(index);
+        // }
+        for (int index = 0; index < 3; index++) {
+            // Map<String, String> filme = listaDeFilmes.get(i);
+            var filme = listaDeFilmes.get(index);
+
+            String urlImagem = filme.get("image");
+            // String urlImagemMaior = urlImagem.replaceFirst("@\\.)([0-9A-Z,,_]+).jpg$",
+            // "$1.jpg");
+            String titulo = filme.get("title");
             double classificao = Double.parseDouble(filme.get("imDbRating"));
-            int numeroEstrelinhas = (int) classificao;
-            for (int n = 1; n <= numeroEstrelinhas; n++) {
-                System.out.print("⭐"); // no terminal para demonstrar será necessário digitar código chcp65001
+
+            String textoFigurinha;
+            InputStream imagemReacao;
+            if (classificao >= 9.1) {
+                textoFigurinha = "TOPZERA";
+                imagemReacao = new FileInputStream(new File("sobreposicao/smiling.jpg"));
+            } else {
+                textoFigurinha = "HMMMM...";
+                imagemReacao = new FileInputStream(new File("sobreposicao/judging.jpg"));
             }
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemReacao);
+
+            System.out.println("\u001b[1mTítulo:\u001b[0m " + titulo);
+            // System.out.println("\u001b[3mURL da imagem:\u001b[0m " + filme.get("image"));
+            // System.out.println(filme.get("imDbRating")); //valor recebido da API
+            // double classificao = Double.parseDouble(filme.get("imDbRating"));
+            // int numeroEstrelinhas = (int) classificao;
+            // for (int n = 1; n <= numeroEstrelinhas; n++) {
+            // System.out.print("⭐"); // no terminal para demonstrar será necessário digitar
+            // código chcp 65001
+            // }
             System.out.println("\n");
         }
     }
